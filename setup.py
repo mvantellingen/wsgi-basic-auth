8e637c4b03ed64eb103c7b44d265671636a2f61d
Michael van Tellingen <michael@mvantellingen.nl>
2016-08-10 21:04:45 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import find_packages, setup
-
+from setuptools import setup
 
 with open('README.rst', 'r') as fh:
     description = '\n'.join(fh.readlines())
+
+docs_require = [
+    'sphinx>=1.4.0',
+]
 
 tests_require = [
     'pytest>=2.6.0',
     'pytest-cov>=1.7.0',
 ]
+
 
 setup(
     name='wsgi-basic-auth',
@@ -20,13 +24,24 @@ setup(
         'webob>=1.0.0',
     ],
     tests_require=tests_require,
-    extras_require={'test': tests_require},
+    extras_require={
+        'docs': docs_require,
+        'test': tests_require,
+    },
     package_dir={'': 'src'},
     py_modules=['wsgi_basic_auth'],
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     zip_safe=False,
 )
